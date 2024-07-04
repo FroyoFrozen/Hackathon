5e34b3f99e8fd814916eb430fafbdb1e0e1578d8
Alex Lamper <lamp0020@hz.nl>
2024-07-04 12:24:32 +0100

    Updated Pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Hackathon - Dashboard</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
