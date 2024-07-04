44ac2dcc397266ba79135866e5ed82a0f5f4f639
Alex Lamper <lamp0020@hz.nl>
2024-07-04 12:25:54 +0100

    Updated Project

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -17,13 +17,10 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/">Dashboard</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+				<a href="/about">Over</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -32,8 +29,8 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
+		<a href="https://github.com/FroyoFrozen/Hackathon">
+			<img src={github} alt="GitHub Project Link" />
 		</a>
 	</div>
 </header>
