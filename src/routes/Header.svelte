c62bd219e3cd9c4bd0dad4d310fa3b221c6beb2c
Tijn Luckerhoff <luck0002@hz.nl>
2024-07-04 14:37:43 +0200

    kleine update"

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -29,7 +29,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/FroyoFrozen/Hackathon">
+		<a target="_blank" href="https://github.com/FroyoFrozen/Hackathon">
 			<img src={github} alt="GitHub Project Link" />
 		</a>
 	</div>
